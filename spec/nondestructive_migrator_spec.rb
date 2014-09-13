b251a1e5d1993fa5100d80add27b83c3c93e7d13
Jason Fleetwood-Boldt <jason@datatravels.com>
2014-09-13 12:17:38 -0400

    adding Rspec to project

---
+++ spec/nondestructive_migrator_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper.rb'
+
+
+describe "NondestructiveMigrator" do
+  it "should be a class" do
+    expect(NondestructiveMigrator).not_to be_nil
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,31 @@
+begin
+  require 'byebug'
+rescue LoadError
+end
+
+$LOAD_PATH << '.' unless $LOAD_PATH.include?('.')
+$LOAD_PATH.unshift(File.expand_path('../lib', __FILE__))
+require 'logger'
+
+require 'rails'
+require 'active_record'
+require 'lib/nondestructive_migrator.rb'
+# require File.expand_path('../lib/nondestructive_migrator.rb', __FILE__)
+# I18n.enforce_available_locales = true
+
+
+# require 'rspec/its'
+# require 'barrier'
+# require 'database_cleaner'
+
+Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
+
+RSpec.configure do |config|
+  config.color = true
+  config.raise_errors_for_deprecations!
+end
+
+
+
+
+
