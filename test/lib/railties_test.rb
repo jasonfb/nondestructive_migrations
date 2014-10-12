15e57e801a454092181995c20a44de931e92c46e
Jason Fleetwood-Boldt <jason@datatravels.com>
2014-10-12 13:50:09 -0400

    Railtie test

---
+++ test/lib/railties_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+require 'minitest/autorun'
+
+puts "hello3"
+
+class DataMigrations::RailtieTest < Minitest::Test
+  def test_that_the_rake_tasks_get_loaded
+    assert true
+
+  end
+end
