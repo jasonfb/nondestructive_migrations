f5e22bf49255402a7977bf3f8bb2163960bbbb9c
Jason Fleetwood-Boldt <jason@mackweldon.com>
2015-02-18 18:51:53 -0500

    require

--- lib/nondestructive_migrations.rb
+++ lib/nondestructive_migrations.rb
@@ -3,6 +3,7 @@ MIGRATIONS_PATH = 'db/data_migrate'
 
 require 'generators/data_migration_install_generator.rb'
 require 'generators/data_migration_generator.rb'
+require 'active_record/data_migration.rb'
 require 'nondestructive_migrator.rb'
 
 module NondestructiveMigrations
